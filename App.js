cb1fca38e19b0ba6084a3975d4ce8fe0042dcf80
pranavnimbagiri <67331631+pranavnimbagiri@users.noreply.github.com>
2021-04-01 16:28:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { StyleSheet, Text, View ,TouchableOpacity,Image} from 'react-native';
+import {ScanScreen} from './screens/ScanScreen'
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
